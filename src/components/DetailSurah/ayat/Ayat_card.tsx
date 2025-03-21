a42e7571390cd5db8c42cf5e5734542b76e9d585
“Sakyy” <sakydgny@gmail.com>
2025-03-21 20:49:38 +0700

    font

--- src/components/DetailSurah/ayat/Ayat_card.tsx
+++ src/components/DetailSurah/ayat/Ayat_card.tsx
@@ -10,7 +10,7 @@ function AyatCard({ ayat }: AyatCardProps) {
     <div className="bg-dark-colore p-4 rounded-lg text-light-colore text-left flex flex-col gap-4">
       <div className="flex justify-between gap-4">
         <Number number={ayat.nomorAyat} />
-        <p className="font-bold text-2xl text-right w-full">{ayat.teksArab}</p>
+        <p className="font-light text-3xl text-right w-full font-[Amiri]">{ayat.teksArab}</p>
       </div>
       <p className="font-sm text-light-colore">{ayat.teksLatin}</p>
       <p className="font-sm text-subtle-colore ">{ayat.teksIndonesia}</p>
