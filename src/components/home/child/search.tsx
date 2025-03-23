7fee7146699f3e3c5a4ad5458a22106a25573205
“Sakyy” <sakydgny@gmail.com>
2025-03-23 21:23:17 +0700

    fix err

--- src/components/home/child/search.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Search = () => {
-  return (
-    <input type="text" className='bg-transparent border border-accent-light-colore px-4 py-2 rounded-full' />
-  )
-}
-
-export default Search
